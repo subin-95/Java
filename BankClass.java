32d462683cc0a692a760cedce7b8d521126dad70
SUBIN LAL S M <107804004+subin-95@users.noreply.github.com>
2022-11-30 22:20:26 +0530

    Add files via upload

---
+++ BankClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank;
+
+/**
+ *
+ * @author lalsu
+ */
+public class BankClass {
+    String bank_name;
+    float interest;
+    void dispay_interest()
+    {
+     
+    System.out.println("The Interest in the "+ bank_name + " is " + interest);
+    }   
+    
+}
