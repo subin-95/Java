4a3e1364e5cc4894228cf05a93a44830c6832434
SUBIN LAL S M <lalsubin3@gmail.com>
2022-12-01 21:18:23 +0530

    SUM

---
+++ sum/src/sum/Sum.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sum;
+
+import static java.time.Clock.system;
+import static java.time.InstantSource.system;
+
+/**
+ *
+ * @author lalsu
+ */
+public class Sum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int a=5;
+   int b=6;
+  int sum=a+b;
+   System.out.println("sum is" +sum);
+            }
+    
+}
