8183657b561c785847e09dc5279ae04557afb9ee
SUBIN LAL S M <lalsubin3@gmail.com>
2022-12-01 21:19:50 +0530

    Argument Constructor

---
+++ Argument_constructor/src/argument_constructor/Argument_constructor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package argument_constructor;
+
+/**
+ *
+ * @author lalsu
+ */
+public class Argument_constructor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Pencil Pencil=new Pencil();
+        System.out.println(Pencil.get_details());
+    }
+    
+}
---
+++ Argument_constructor/src/argument_constructor/Pencil.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package argument_constructor;
+
+/**
+ *
+ * @author lalsu
+ */
+public class Pencil {
+   
+    String name;
+    String shape;
+    float length;
+    float dia;
+    Pencil(){
+        name="Apsara";
+        shape="Triangle";
+        length=15;
+        dia=10;
+    }
+    Pencil(String name1,String shape1,float length1,float dia1){
+        name1="Natraj";
+        shape1="Hexagonal";
+        length1=15;
+        dia1=10;
+    }
+    String get_details(){
+        return("Name of the pencil is "+name+" and is of "+shape+" shape in cross section. The length and dia of the pencil is "+length+" cm and "+dia+" mm");
+    }
+}
+    
+
